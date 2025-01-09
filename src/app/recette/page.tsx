58e85b7fab2283e15c17203d9ef60e7f37ad79ba
v-alex-dev <vensalex1991@gmail.com>
2025-01-09 12:04:29 +0100

    Adjust style on input

--- src/app/recette/page.tsx
+++ src/app/recette/page.tsx
@@ -8,7 +8,7 @@ export default function Recette() {
           <form className="mt-2">
             <div className="">
               <label htmlFor="title">Nom de la recette</label>
-                <input className="rounded-full" type="text" id="title" name="title" required />
+                <input  type="text" id="title" name="title" required />
             </div> 
           </form>
         </section>
