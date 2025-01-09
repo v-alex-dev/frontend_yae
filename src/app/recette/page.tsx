d774619d4be5e24f91934f28e38021c865c5671a
v-alex-dev <vensalex1991@gmail.com>
2025-01-09 12:04:11 +0100

    Add component

--- src/app/recette/page.tsx
+++ src/app/recette/page.tsx
@@ -4,7 +4,7 @@ export default function Recette() {
     return(
         <section className="container mx-auto p-4 flex flex-col items-center">
           <h2>Création d'une recette</h2>
-
+          <AddIngredientForm />          
           <form className="mt-2">
             <div className="">
               <label htmlFor="title">Nom de la recette</label>
